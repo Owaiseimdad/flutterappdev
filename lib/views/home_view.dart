ccf7a7b6edd0593619c6be6b2bafb9b9c337a139
Owaise Imdad <owaiseimdad@gmail.com>
2024-03-07 22:59:53 +0530

    added logic in home page in order to show to render the right page.

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:learning_free_code_camp/firebase_options.dart';
 import 'package:learning_free_code_camp/views/login_view.dart';
+import 'package:learning_free_code_camp/views/verify_view.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -15,14 +17,18 @@ class HomePage extends StatelessWidget {
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
-              // final user = FirebaseAuth.instance.currentUser;
-              // print(user?.emailVerified);
-              // if (user?.emailVerified ?? false) {
-              //   return const Text("You are a verified user.");
-              // } else {
-              //   return const VerifyEmail();
-              // }
-              return const LoginView();
+              final user = FirebaseAuth.instance.currentUser;
+              print(user?.emailVerified);
+              if (user != null) {
+                if (user.emailVerified) {
+                  return const Text("You are a verified user.");
+                } else {
+                  return const VerifyEmail();
+                }
+              } else {
+                return const LoginView();
+              }
+
             default:
               return const CircularProgressIndicator();
           }
